553009b8d91c18224d6c69cc02c291c9110f0500
pedro souza <pedronina424@gmail.com>
2026-05-12 08:34:37 -0300

    Add files via upload

---
+++ Lagosta Store/js/script.js
@@ -0,0 +1,16 @@
+function verificarLogin(){
+    let login = document.getElementById("login").value;
+    let senha = document.getElementById("senha").value;
+
+    if(login === "admin" && senha === "123456"){
+        alert("Acesso Liberado!");
+        window.location.href="Produtos.html";
+    } else {
+        alert("Acesso Negado!")
+    }
+
+    login = document.getElementById("login").value = '';
+    senha = document.getElementById("senha").value = '';
+
+
+}
